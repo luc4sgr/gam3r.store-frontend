e0cad762936aa082528d5defc627621fec7aae1c
lucasgranjense23 <23.lucasdoliveira@gmail.com>
2024-09-30 23:10:50 -0300

    Feat:pagina inicial e rota de produto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import ProdutoItem from "@/components/produto/ProdutoItem";
+import { produtos } from "@/core";
+
 export default function Home() {
   return (
-   <div>Aplicação</div>
+   <div className="grid grid-cols-4">
+    {
+      produtos.map((produto) => (
+        <ProdutoItem key={produto.id} produto={produto}/> 
+      ))
+    }
+    </div>
   );
 }
---
+++ src/app/produto/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function PaginaProduto() {
+    return (
+        <div>
+            <h1>Produto</h1>
+        </div>
+    );
+}
